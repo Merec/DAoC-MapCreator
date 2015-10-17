78ffb2a99db2203cf975253b1fd970d4615db554
Stefan Schaefer <merec@merec.org>
2015-10-17 18:41:34 +0200

    Localized assembly file

--- MapCreator/Properties/AssemblyInfo.cs
+++ MapCreator/Properties/AssemblyInfo.cs
@@ -3,36 +3,16 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Resources;
 
-// Allgemeine Informationen über eine Assembly werden über die folgenden 
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die mit einer Assembly verknüpft sind.
 [assembly: AssemblyTitle("MapCreator")]
-[assembly: AssemblyDescription("Create maps for Dark Age of Camelot")]
+[assembly: AssemblyDescription("Generate Dark Age of Camelot (DAoC) maps using the game client.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MapCreator")]
-[assembly: AssemblyCopyright("(c) Merec 2014")]
+[assembly: AssemblyCopyright("(c) Merec 2015")]
 [assembly: AssemblyTrademark("Dawn of Light")]
 [assembly: AssemblyCulture("")]
-
-// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
-// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly von 
-// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
 [assembly: ComVisible(false)]
-
-// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("aa6c129e-6bad-4fce-bc5d-a9a6b3f299ed")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.1")]
 [assembly: AssemblyFileVersion("1.3.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
