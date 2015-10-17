5f59e42e5a3e101abaf04b2e94af7c9ac9d453f7
Stefan Schaefer <merec@merec.org>
2015-10-17 17:26:16 +0200

    Prepare github release

--- MapCreator/Properties/AssemblyInfo.cs
+++ MapCreator/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Resources;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
